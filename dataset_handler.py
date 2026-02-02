ae47dfa94ddc33581ab46f6cab16c017bc31e4cb
Jonas Birkeli <jonas.birkeli@icloud.com>
2026-02-02 12:56:14 +0100

    DOCS: Add source of dataset

--- dataset_handler.py
+++ dataset_handler.py
@@ -1,3 +1,5 @@
 from datasets import load_dataset
 
+# Retrieved from:
+# https://huggingface.co/datasets/google/code_x_glue_cc_clone_detection_big_clone_bench
 ds = load_dataset("google/code_x_glue_cc_clone_detection_big_clone_bench")
